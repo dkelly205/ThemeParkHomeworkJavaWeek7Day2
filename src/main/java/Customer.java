95bfa74c947ae846fe4aca8926b20f426de23cca
daniel kelly <danieljkelly91@gmail.com>
2018-03-06 16:32:16 +0000

    added customer class

---
+++ src/main/java/Customer.java
@@ -0,0 +1,21 @@
+public class Customer {
+
+
+    private double cash;
+    private double height;
+    private int age;
+    private String name;
+    private Band band;
+
+    public Customer(String name, int age, double height, double cash, Band band) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.cash = cash;
+        this.band = band;
+
+    }
+
+
+
+}
