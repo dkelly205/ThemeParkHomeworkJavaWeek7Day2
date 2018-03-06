8212f606d493db6d36139f41fabe61557f0da09c
daniel kelly <danieljkelly91@gmail.com>
2018-03-06 21:13:34 +0000

    added till property to rollercoaster

--- src/main/java/Rollercoaster.java
+++ src/main/java/Rollercoaster.java
@@ -1,10 +1,12 @@
 public class Rollercoaster extends Ride {
 
     private double cost;
+    private double till;
 
     public Rollercoaster(String name, int minAge, double minHeight, int capacity, double cost) {
         super(name, minAge, minHeight, capacity);
         this.cost = cost;
+        this.till = 0.00;
 
     }
 
